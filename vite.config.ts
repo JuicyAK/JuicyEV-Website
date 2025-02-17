fe0049f33ac61de1b310ba30542c59bf7aeb2965
JuicyAK <ashwinidaskaranth@gmail.com>
2025-02-17 15:29:09 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/JuicyEV-Website/' // Change this to match your GitHub repo name
+});
